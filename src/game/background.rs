54b648cbc3418a01ced8509d9dc272da3d3f5cfa
Frank Hampus Weslien <frankhampusweslien@google.com>
2023-10-28 23:18:51 +0200

    Add generic background tileing

--- src/game/background.rs
+++ src/game/background.rs
@@ -2,6 +2,10 @@ use bevy::{prelude::*, window::PrimaryWindow};
 
 use super::camera::ShakyCamera;
 
+const BACKGROUND_TILE_WIDTH: f32 = 256.0;
+const BACKGROUND_TILE_HEIGHT: f32 = 256.0;
+const BACKGROUND_TILES_SIZE: u32 = 3;
+
 ////////////////////////////////////////////////////////////////////////////////
 /// Plugin
 ////////////////////////////////////////////////////////////////////////////////
@@ -11,8 +15,15 @@ pub struct BackgroundPlugin;
 
 impl Plugin for BackgroundPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_background)
-            .add_systems(Update, (update_background, update_tile_debug));
+        app.insert_resource(BackgroundGrid {
+            grid: Grid::new(
+                BACKGROUND_TILES_SIZE,
+                BACKGROUND_TILE_WIDTH,
+                BACKGROUND_TILE_HEIGHT,
+            ),
+        })
+        .add_systems(Startup, spawn_background)
+        .add_systems(Update, (update_background, update_tile_debug));
     }
 }
 
@@ -20,27 +31,14 @@ impl Plugin for BackgroundPlugin {
 /// Component
 ////////////////////////////////////////////////////////////////////////////////
 
-// The background that the player will see. It's just a sprite that's
-// scaled to the size of the window, and shows distant stars.
 #[derive(Component)]
-pub struct Background;
-
-#[derive(Component)]
-pub struct BackgroundCenterTile;
-
-#[derive(Component)]
-pub struct BackgroundTile {
-    pub x: i32,
-    pub y: i32,
+struct BackgroundTile {
+    pub tile: Tile,
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 /// System
 ////////////////////////////////////////////////////////////////////////////////
-
-const BACKGROUND_TILE_WIDTH: f32 = 256.0;
-const BACKGROUND_TILE_HEIGHT: f32 = 256.0;
-const BACKGROUND_TILES_SIZE: u32 = 3;
 
 // Spawn the background
 pub fn spawn_background(
@@ -81,10 +79,8 @@ pub fn spawn_background(
 
     for x in low_bound..=high_bound {
         for y in low_bound..=high_bound {
-            let position = background_tile_position(x, y);
             let transform = Transform::from_xyz(
-                position.x, position.y,
-                -1.0, // The z axis is used to place the background behind everything
+                0.0, 0.0, -1.0, // The z axis is used to place the background behind everything
             );
 
             // transform.scale = Vec3::new(
@@ -99,7 +95,9 @@ pub fn spawn_background(
                     texture: background_handle.clone(),
                     ..Default::default()
                 })
-                .insert(BackgroundTile { x, y });
+                .insert(BackgroundTile {
+                    tile: Tile::new(x, y),
+                });
         }
     }
 }
@@ -115,49 +113,10 @@ fn update_tile_debug(mut gizmos: Gizmos, mut query: Query<&Transform, With<Backg
     }
 }
 
-fn background_tile_position(x: i32, y: i32) -> Vec2 {
-    Vec2::new(
-        x as f32 * BACKGROUND_TILE_WIDTH,
-        y as f32 * BACKGROUND_TILE_HEIGHT,
-    )
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_background_tile_position() {
-        let pos = background_tile_position(0, 0);
-        assert_eq!(pos, Vec2::new(0.0, 0.0));
-
-        let pos = background_tile_position(1, 1);
-        assert_eq!(
-            pos,
-            Vec2::new(BACKGROUND_TILE_WIDTH, BACKGROUND_TILE_HEIGHT)
-        );
-
-        let pos = background_tile_position(-2, 3);
-        assert_eq!(
-            pos,
-            Vec2::new(BACKGROUND_TILE_WIDTH * -2.0, BACKGROUND_TILE_HEIGHT * 3.0)
-        );
-    }
-}
-
-// Imagine the entire world is a grid of tiles. The tile the player is
-// currently on is the center tile. This function returns the center
-// tile's position.
-fn background_center(camera_position: Vec3) -> Vec2 {
-    let x = (camera_position.x / BACKGROUND_TILE_WIDTH).floor() as i32;
-    let y = (camera_position.y / BACKGROUND_TILE_HEIGHT).floor() as i32;
-
-    background_tile_position(x, y)
-}
-
 // The background follows the camera, but at a slower rate
-pub fn update_background(
+fn update_background(
     // window_query: Query<&Window, With<PrimaryWindow>>,
+    background_grid: Res<BackgroundGrid>,
     mut query_background: Query<(&mut Transform, &BackgroundTile), Without<ShakyCamera>>,
     query_camera: Query<&GlobalTransform, (Without<BackgroundTile>, With<ShakyCamera>)>,
 ) {
@@ -166,16 +125,114 @@ pub fn update_background(
     // let window_height = window.height();
 
     if let Ok(camera_transform) = query_camera.get_single() {
-        let tile_center = background_center(camera_transform.translation());
+        let tile = background_grid
+            .grid
+            .tile(camera_transform.translation().truncate());
+        let tile_center = background_grid.grid.tile_position(tile);
 
         for (mut transform, background_tile) in query_background.iter_mut() {
-            let relative_tile_position =
-                background_tile_position(background_tile.x, background_tile.y);
+            let relative_tile_position = background_grid.grid.tile_position(background_tile.tile);
             let absolute_tile_position = tile_center + relative_tile_position;
 
             transform.translation.x = absolute_tile_position.x;
             transform.translation.y = absolute_tile_position.y;
             transform.translation.z = -1.0;
         }
+    }
+}
+////////////////////////////////////////////////////////////////////////////////
+/// Background Grid
+///////////////////////////////////////////////////////////////////////////////
+
+#[derive(Resource)]
+struct BackgroundGrid {
+    grid: Grid,
+}
+
+// A tile in the background grid
+
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Tile {
+    x: i32, // x position in the grid
+    y: i32, // y position in the grid
+}
+
+impl Tile {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+}
+
+// The background grid
+#[derive(Default, Debug, Clone, Copy, PartialEq)]
+pub struct Grid {
+    size: u32, // number of tiles in each direction, so a size of 3 means 3x3 tiles. Must be odd.
+    tile_width: f32,
+    tile_height: f32,
+}
+
+impl Grid {
+    fn new(size: u32, tile_width: f32, tile_height: f32) -> Self {
+        assert!(size % 2 == 1);
+        Self {
+            size,
+            tile_width,
+            tile_height,
+        }
+    }
+
+    fn center(&self) -> Tile {
+        Tile::default()
+    }
+
+    fn tile_position(&self, tile: Tile) -> Vec2 {
+        self.tile_position_xy(tile.x, tile.y)
+    }
+
+    fn tile_position_xy(&self, x: i32, y: i32) -> Vec2 {
+        Vec2::new(x as f32 * self.tile_width, y as f32 * self.tile_height)
+    }
+
+    /// Returns the tile that the position is in
+    fn tile(&self, position: Vec2) -> Tile {
+        self.tile_xy(position.x, position.y)
+    }
+
+    fn tile_xy(&self, x: f32, y: f32) -> Tile {
+        let x = (x / self.tile_width).floor() as i32;
+        let y = (y / self.tile_height).floor() as i32;
+
+        Tile::new(x, y)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tile_position() {
+        let grid = Grid::new(3, 10.0, 10.0);
+        let pos = grid.tile_position_xy(0, 0);
+        assert_eq!(pos, Vec2::new(0.0, 0.0));
+
+        let pos = grid.tile_position_xy(1, 1);
+        assert_eq!(pos, Vec2::new(10.0, 10.0));
+
+        let pos = grid.tile_position_xy(-2, 3);
+        assert_eq!(pos, Vec2::new(-20.0, 30.0));
+    }
+
+    #[test]
+    fn test_background_tile() {
+        let grid = Grid::new(3, 10.0, 10.0);
+        let camera_position = Vec2::new(0.0, 0.0);
+        assert_eq!(grid.tile(camera_position), Tile::new(0, 0));
+
+        let camera_position = Vec2::new(100.0, 100.0);
+        assert_eq!(grid.tile(camera_position), Tile::new(10, 10));
+
+        let camera_position = Vec2::new(-50.0, -50.0);
+        assert_eq!(grid.tile(camera_position), Tile::new(-5, -5));
     }
 }
