8ba2b78cb20f9f25a1563a7ed0dcbfed8c10998b
Frank Hampus Weslien <frankhampusweslien@google.com>
2023-11-13 20:11:47 +0100

    Add find_unobstructured_path

--- src/misc/rapier_extension.rs
+++ src/misc/rapier_extension.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::{Color, Entity, Gizmos, Transform, Vec2, Vec3};
-use bevy_rapier2d::prelude::{QueryFilter, RapierContext};
+use bevy_rapier2d::prelude::{Collider, QueryFilter, RapierContext};
 use std::collections::HashMap;
 
 /// Cast a vision cone from the given entity. Returns a map of entities to the average position of
@@ -39,7 +39,7 @@ pub fn cast_vision_cone(
     let cone_direction = (entity_transform.rotation * Vec3::Y).truncate();
 
     while ray_angle < angle / 2.0 {
-        let direction = cone_direction.rotate(Vec2::new(ray_angle.cos(), ray_angle.sin()));
+        let direction: Vec2 = cone_direction.rotate(Vec2::new(ray_angle.cos(), ray_angle.sin()));
 
         let ray_start = start + direction * inner_distance;
 
@@ -85,7 +85,8 @@ pub fn cast_vision_cone(
 ///
 /// It works by performing shape casting (that is why you give it a width) and returns the first path
 /// it finds that is clear of obstacles. We use a shape casting to make sure the path is wide enough
-/// for the entity to fit through.
+/// for the entity to fit through. If there are multiple paths, the one closest to the entity's
+/// current direction is returned.
 ///
 /// If no path is found, None is returned.
 ///
@@ -94,22 +95,62 @@ pub fn cast_vision_cone(
 /// * `ctx`: The rapier context
 /// * `entity_transform`: The transform of the entity casting the vision cone
 /// * `ray_angel_density`: The number of rays to cast per degree
-/// * `inner_distance`: The distance from the entity to start casting rays
-/// * `outer_distance`: The distance from the entity to stop casting rays
+/// * `inner_distance`: The distance from the entity to start shape casting
+/// * `outer_distance`: The distance from the entity to stop shape casting
 /// * `width`: The width of the entity
+/// * `filter`: The filter to use when shape casting. This allows you to ignore certain entities.
+/// * `max_angle`: The maximum angle we will shape cast for (angels are given in radians)
 ///
 /// # Returns
 ///
 /// The best angle to turn so the entity can take to avoid the obstacles in
 /// its way. None if no angle is found.
 ///
-pub fn find_clear_path(
+pub fn find_unobstructed_path(
     ctx: &RapierContext,
     entity_transform: &Transform,
     ray_angel_density: f32,
     inner_distance: f32,
     outer_distance: f32,
     width: f32,
-) -> Option<f32> {
-    None
+    filter: QueryFilter,
+    max_angle: Option<f32>,
+) -> Option<Vec2> {
+    let entity_pos = entity_transform.translation.truncate();
+    let entity_direction = (entity_transform.rotation * Vec3::Y).truncate();
+
+    let shape = Collider::cuboid(width / 2.0, 1.0);
+    let max_toi = outer_distance - inner_distance;
+
+    let check_angle = |angle: f32| -> Option<Vec2> {
+        let direction_vec: Vec2 = entity_direction
+            .rotate(Vec2::new(angle.cos(), angle.sin()))
+            .normalize();
+        let shape_rot = direction_vec.y.atan2(direction_vec.x);
+
+        let start_pos = entity_pos + direction_vec * inner_distance;
+
+        let res = ctx.cast_shape(start_pos, shape_rot, direction_vec, &shape, max_toi, filter);
+
+        if let None = res {
+            return Some(direction_vec);
+        }
+        return None;
+    };
+
+    let max_angle_offset = max_angle.unwrap_or(std::f32::consts::PI);
+    let mut current_angle_offset = 0.0;
+
+    // Start shape casting at the current angle and work our way outwards
+    while current_angle_offset < max_angle_offset {
+        if let res @ Some(_) = check_angle(current_angle_offset) {
+            return res;
+        }
+        if let res @ Some(_) = check_angle(-1.0 * current_angle_offset) {
+            return res;
+        }
+        current_angle_offset += 1.0 / ray_angel_density;
+    }
+
+    return None;
 }
